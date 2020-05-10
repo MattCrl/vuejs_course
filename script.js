96b972bdde160824b9730235ca93806cfee653ca
MattCrl <matth.carlevaris@gmail.com>
2020-05-10 22:44:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var myObject = new Vue({
+  el: "#app",
+  data: {
+    h: 'HH',
+    m: 'MM',
+    s: 'SS'
+  },
+})
+
+setInterval(function() {
+  var myDate = new Date()
+  myObject.h = ("0" + myDate.getHours()).slice(-2)
+  myObject.m = ("0" + myDate.getMinutes()).slice(-2)
+  myObject.s = ("0" + myDate.getSeconds()).slice(-2)
+}, 1000)
